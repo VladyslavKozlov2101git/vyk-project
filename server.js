daf3cbdb10acac8445daa2e6d8811905be8416f1
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2021-06-14 21:52:41 +0300

    create server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const connectDB = require('./config/db')
+
+
+// Connect DB
+
+connectDB()
+
+//Init middleWare
+
+app.use(express.json())  //это метод, встроенный в express для распознавания входящего объекта запроса как объекта JSON. Этот метод вызывается в качестве промежуточного программного обеспечения в вашем приложении с использованием кода
+
+
+app.get('/', (req, res)=> res.send('API running'))
+
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'))
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=> console.log(`Server has been started on PORT ${PORT}`))
