93dcf9790b48426614b91602a7f09f73b01e5d23
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2021-06-23 22:41:15 +0300

    Create get/ get all / delete requests for todos

--- routes/api/todo.js
+++ routes/api/todo.js
@@ -8,8 +8,10 @@ const User = require('../../models/User')
 const Todo = require('../../models/Todo')
 const bcrypt = require('bcryptjs');
 
+
+
 // @route   POST api/todo
-// @desc    Create new post
+// @desc    Create new todo item
 // @access  Private
 
 router.post('/',[auth,[
@@ -51,7 +53,7 @@ router.post('/',[auth,[
 
         const todo = await newTodo.save()
 
-        res.json(todo)
+        res.status(201).json(todo)
 
         
     } catch (error) {
@@ -64,5 +66,167 @@ router.post('/',[auth,[
 
 );
 
+
+// @route   GET api/todo/
+// @desc    Get  all todo items by user id
+// @access  Private
+
+router.get('/', auth, async (req, res)=>{
+    try {
+        const posts = await Todo.find().sort({date:-1})
+        res.status(200).json({total:posts.length, posts})
+        
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server error')
+    }
+})
+
+
+// @route   GET api/todo/:id
+// @desc    Get  todo item by id
+// @access  Private
+
+
+router.get('/:id', auth, async (req, res)=>{
+    try {
+        const todo = await Todo.findById(req.params.id)
+
+        if(!todo){
+            return res.status(404).json({msg:'Todo item not found'})
+        }
+
+        res.json(todo)
+        
+    } catch (error) {
+        console.error(error.message)
+        if(err.kind === 'ObjectId'){
+            return res.status(404).json({msg:'Todo item not found'})
+        }
+        res.status(500).send('Server error')
+    }
+})
+
+
+// @route   Delete api/todo/:id
+// @desc    Delete todo item by id
+// @access  Private
+
+
+router.delete('/:id', auth, async (req, res)=>{
+    try {
+        const todo = await Todo.findById(req.params.id)
+
+        if(!todo){
+            return res.status(404).json({msg:'Todo item is not found'})
+        }
+       
+        //Check user
+
+        if(todo.user.toString() !== req.user.id){
+            return res.status(401).json({msg:'User not authorized'})
+        }
+        
+        await todo.remove()
+
+        res.json({msg:'Todo item is removed'})
+
+        
+    } catch (error) {
+        console.error(error.message)
+        if(err.kind === 'ObjectId'){
+            return res.status(404).json({msg:'Todo item is not found'})
+        }
+        res.status(500).send('Server error')
+    }
+})
+
+
+
+// @route   Patch api/todo/:id
+// @desc    Update user's info  by id
+// @access  Private
+
+router.patch(
+    '/:id', [auth, [
+        check('title', 'Title is required')
+            .not()
+            .isEmpty(),
+        check('description', 'Description is required')
+            .not()
+            .isEmpty(),
+        check('category', 'Can only be one of the enum values')
+            .isIn(['shopping', 'study', 'recreation']),
+        check('priority', 'Can only be one of the enum values')
+            .isIn(['low', 'medium', 'hight'])
+  ]],  async (req, res) => {
+  
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+      }
+
+      const{
+        title,
+        description,
+        isImportant,
+        priority,
+        category,
+        doDate,
+        isDone
+
+    } = req.body
+
+
+    const todoFields = {}
+
+    todoFields.user = req.user.id
+
+    if(title) todoFields.title = title
+    if(description) todoFields.description = description
+    if(isImportant) todoFields.isImportant = isImportant
+    if(priority) todoFields.priority = priority
+    if(category) todoFields.category = category
+    if(doDate) todoFields.doDate = doDate
+    if(isDone) todoFields.isDone = isDone
+
+
+
+
+
+
+
+  
+      try {
+        let todo = await Todo.findById(req.params.id)
+        
+
+        //Update
+
+        if(todo){
+            todo = await User.findOneAndUpdate(
+                {"title": title},
+                {"description": description},
+                {"isImportant": isImportant},
+                {"priority": priority},
+                {"category": category},
+                {"doDate": doDate},
+                {"isDone": isDone}
+            )
+
+            return res.json(todo)
+        }
+  
+        // todo = new Profile(todoFields)
+        // await todo.save()
+        // res.json(todo)
+
+  
+      } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error1')
+      }
+    }
+  )
 
 module.exports = router;
